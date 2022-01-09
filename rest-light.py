d59640856b6a18fad1d04eb37a5d4a12833a6f1b
Pascal Höhnel <20238923+uupascal@users.noreply.github.com>
2022-01-09 20:50:24 +0100

    improve logging & performance

--- rest-light.py
+++ rest-light.py
@@ -35,7 +35,7 @@ def setup_logging():
 
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(log_level)
-    formatter = logging.Formatter('[%(asctime)s] - %(levelname)s: %(message)s')
+    formatter = logging.Formatter('[%(asctime)s  ] - %(levelname)s: %(message)s')
     handler.setFormatter(formatter)
     root.addHandler(handler)
 
